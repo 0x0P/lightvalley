c4307c543caab9f3dc31a909629bf256e98e92d4
PERCENTZERO <percentzero@outlook.com>
2023-03-24 00:55:02 +0900

    create express server

---
+++ db.sql
@@ -0,0 +1,11 @@
+create user lightvalley@localhost;
+grant all privileges on lightvalley.* to lightvalley@localhost;
+create database lightvalley;
+use lightvalley;
+
+create table documents (
+    version number
+    name text,
+    fakeName text,
+    content text,
+)
---
+++ src/types/document.ts
@@ -0,0 +1,4 @@
+interface Document {
+  name: string;
+  version: number;
+}
