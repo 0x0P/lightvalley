27b9a712b87102e23274b3dd52c29b7cb683face
PERCENTZERO <percentzero@outlook.com>
2023-03-25 22:13:39 +0900

    라우터 분리

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import docs from "./router/docs/index";
 
 const app = express();
 const port = 3000;
@@ -6,6 +7,8 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("#LightValley");
 });
+
+app.use("/docs", docs);
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
