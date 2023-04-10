64644bc54aeaea038dfa5aaf940e62940f111cb5
PERCENTZERO <percentzero@outlook.com>
2023-04-11 00:29:36 +0900

    계정 생성 구현

--- src/router/auth/create.ts
+++ src/router/auth/create.ts
@@ -1,7 +1,28 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { db } from "../../global";
+import bcrypt from "bcrypt";
+import { User, permission } from "../../types/user";
+
 const router = Router();
 
-//todo
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    const { name, pw } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(pw, salt);
+    const user: User = {
+      name,
+      password,
+      salt,
+      time: new Date(),
+      permission: permission.user,
+    };
+    await db("users").insert(user);
+    res.status(201).json({ message: "계정이 생성되었습니다." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
+});
 
 export default router;
