02cf808dbbfe7d7d69383cfb7909a75b6ef0ff78
PERCENTZERO <percentzero@outlook.com>
2023-04-10 18:38:42 +0900

    타입 수정 - 권한

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,7 +1,14 @@
-interface User {
+export interface User {
   name: string;
   password: string;
   salt: string;
   time: Date;
-  permissions: number;
+  permission: permission;
+}
+
+export enum permission {
+  admin = 1,
+  user = 2,
+  guest = 3,
+  block = 4,
 }
