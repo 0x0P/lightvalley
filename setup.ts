2f0a2014792d63312323e9cb6c03f555ae22ec43
PERCENTZERO <percentzero@outlook.com>
2023-04-13 19:28:37 +0900

    PRIVATE 및 PUBLIC 키 생성

--- setup.ts
+++ setup.ts
@@ -1,1 +1,11 @@
-//todo
+import fs from "fs";
+
+function makeKey(num: number) {
+  let text = "";
+  for (let i = 0; i < num; i++) {
+    text += String.fromCharCode(Math.floor(Math.random() * 65535));
+  }
+  return text;
+}
+
+fs.writeFileSync(".env", `PUBLIC=${makeKey(1000)}\nPRIVATE=${makeKey(1000)}`);
