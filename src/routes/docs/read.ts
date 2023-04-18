7f314098fabb4bc22358adf400f67a9a7c7857e5
PERCENTZERO <percentzero@outlook.com>
2023-04-19 06:31:32 +0900

    문서 읽기 구현

--- src/routes/docs/read.ts
+++ src/routes/docs/read.ts
@@ -1,7 +1,22 @@
 import { Router } from "express";
 import { db } from "../../global";
 const router = Router();
+import { Document } from "../../types/document";
 
-//todo
-
+router.get("/:name", async (req, res) => {
+  try {
+    const doc: Document = await db("documents")
+      .select()
+      .where("name", req.params.name)
+      .first();
+    if (doc) {
+      res.status(200).json({ ok: true, doc });
+    } else {
+      res.status(404).json({ ok: false, error: "문서를 찾을 수 없습니다." });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ ok: false, error });
+  }
+});
 export default router;
