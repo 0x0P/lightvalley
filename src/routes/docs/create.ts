a6e2b823247b036aa66fc40c12f955ff9a458a5a
PERCENTZERO <percentzero@outlook.com>
2023-04-19 05:23:36 +0900

    Merge branch 'main' of https://github.com/0x0P/lightvalley

--- src/routes/docs/create.ts
+++ src/routes/docs/create.ts
@@ -1,32 +1,31 @@
 import { Router } from "express";
 import { db } from "../../global";
-const router = Router();
 import { Document, documentTypes } from "../../types/document";
+import { createDocumentReqBody } from "../../types/req";
+import * as crypto from "crypto";
+import { makeKey } from "../../utils/makeKey";
+
+const router = Router();
 router.post("/", async (req, res) => {
   try {
-    const {
-      name,
-      displayName,
-      content,
-      type,
-    }: {
-      name: string;
-      displayName: string;
-      content: string;
-      type: documentTypes;
-    } = req.body;
+    const { name, displayName, content, type }: createDocumentReqBody =
+      req.body;
     if (![name, displayName, content, type].every(Boolean))
       return res.status(400).json({ ok: false });
+    const hash = crypto.createHash("sha256");
+    hash.update(req.ip + makeKey(10));
+
     const document: Document = {
       version: 1,
       type: documentTypes[type],
-      author: req.ip,
+      author: hash.digest("hex"),
       name,
       displayname: displayName,
       content,
       read: 0,
       edit: 0,
     };
+
     await db("documents").insert(document);
     res.status(200).json({ ok: true, document: document });
   } catch (error) {
