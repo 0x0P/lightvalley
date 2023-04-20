67892153e55ff74a849910b7a12b5edf66a62d39
PERCENTZERO <percentzero@outlook.com>
2023-04-21 01:49:54 +0900

    verify 함수 제거

--- src/utils/genAccess.ts
+++ src/utils/genAccess.ts
@@ -1,13 +1,6 @@
-import { authReqbody } from "../types/req";
-import { genJwtToken, verifyJwtToken } from "./genJwt";
-import * as dotenv from "dotenv";
+import { ReqUser } from "../types/req";
+import { genJwtToken } from "./genJwt";
 
-dotenv.config();
-
-export const genAccessToken = (user: authReqbody) => {
-  return genJwtToken(user, process.env.ACCESS ?? "", "15m");
-};
-
-export const verifyAccessToken = (accessToken: string) => {
-  return verifyJwtToken(accessToken, process.env.ACCESS ?? "");
+export const genAccessToken = (user: ReqUser) => {
+  return genJwtToken(user, process.env.ACCESS as string, "15m");
 };
