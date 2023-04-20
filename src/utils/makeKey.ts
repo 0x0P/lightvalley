7a3e3f5bc6dd793af1d88dad7b05b1bf37353e32
PERCENTZERO <percentzero@outlook.com>
2023-04-20 19:21:03 +0900

    ㅇㄹㄹ

--- src/utils/makeKey.ts
+++ src/utils/makeKey.ts
@@ -1,7 +1,7 @@
-export function makeKey(num: number) {
+export const makeKey = (num: number) => {
   let text = "";
   for (let i = 0; i < num; i++) {
     text += String.fromCharCode(Math.floor(Math.random() * 65535));
   }
   return text;
-}
+};
