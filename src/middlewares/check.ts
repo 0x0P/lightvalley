092fd94feb0c0c1dcdb61e1b1a1992853b5d2e69
PERCENTZERO <percentzero@outlook.com>
2023-04-21 01:49:19 +0900

    체크 미들웨어 생성

--- src/middlewares/check.ts
+++ src/middlewares/check.ts
@@ -1,6 +1,14 @@
 import { Request, Response, NextFunction } from "express";
+import { verifyJwtToken } from "../utils/genJwt";
 
 export const checkAuth = (req: Request, res: Response, next: NextFunction) => {
-  //todo
+  const accessToken = req.cookies["AccessToken"];
+  const decoded = verifyJwtToken(
+    accessToken,
+    process.env.ACCESS as string,
+    res,
+    req.cookies
+  );
+  req.user = decoded;
   next();
 };
