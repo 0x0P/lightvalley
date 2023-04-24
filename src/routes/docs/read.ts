039572d994ca83c643468d4a29041e33b307bf02
PERCENTZERO <percentzero@outlook.com>
2023-04-24 19:18:33 +0900

    문서 읽기 구조 변경

--- src/routes/docs/read.ts
+++ src/routes/docs/read.ts
@@ -1,23 +1,23 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { db } from "../../global";
 const router = Router();
 import { Document } from "../../types/document";
-import { checkAuth } from "../../middlewares/check";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { checkPermission } from "../../utils/checkPermission";
 
-router.get("/:name", checkAuth, async (req, res) => {
-  try {
-    const doc: Document = await db("documents")
-      .select()
-      .where("name", req.params.name)
-      .first();
-    if (doc) {
+router.get("/:name", checkAuth, async (req: Request, res: Response) => {
+  const doc: Document = await db("documents")
+    .select()
+    .where("name", req.params.name)
+    .first();
+  if (doc) {
+    if (await checkPermission(req.user.id, doc.read)) {
       res.status(200).json({ ok: true, doc });
     } else {
-      res.status(404).json({ ok: false, error: "문서를 찾을 수 없어요." });
+      res.status(403).json({ ok: false, error: "DOCS:2" });
     }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ ok: false, error });
+  } else {
+    res.status(404).json({ ok: false, error: "DOCS:1" });
   }
 });
 export default router;
