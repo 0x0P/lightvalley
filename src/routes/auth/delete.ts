02292e8a8aa9d1dec60f60f0c97e71b828136390
PERCENTZERO <percentzero@outlook.com>
2023-04-24 19:16:04 +0900

    계정 삭제 구현

--- src/routes/auth/delete.ts
+++ src/routes/auth/delete.ts
@@ -1,7 +1,36 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { db } from "../../global";
+import { User } from "../../types/user";
+import bcrypt from "bcrypt";
+import { checkAuth } from "../../middlewares/checkAuth";
 const router = Router();
 
-//todo
+router.delete("/", checkAuth, async (req: Request, res: Response) => {
+  try {
+    const { pw } = req.body;
+    if (!pw) return res.status(400).json({ ok: false, error: "Request:1" });
+    const user: User = await db("users")
+      .select()
+      .where("id", req.user.id)
+      .first();
+    bcrypt.compare(pw, user.password).then(function (result) {
+      if (result) {
+        db("users")
+          .where("id", req.user.id)
+          .del()
+          .then(() => {
+            res.clearCookie("AccessToken");
+            res.clearCookie("RefreshToken");
+            res.send({ ok: true });
+          });
+      } else {
+        res.send({ ok: false, error: "Auth:2" });
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ ok: false, error: error });
+  }
+});
 
 export default router;
