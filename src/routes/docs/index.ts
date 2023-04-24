62543e24b5d0a86e6b32b3492e222930ff20f890
PERCENTZERO <percentzero@outlook.com>
2023-04-24 19:16:48 +0900

    라우터 추가

--- src/routes/docs/index.ts
+++ src/routes/docs/index.ts
@@ -4,5 +4,6 @@ const router = Router();
 router.use("/read", require("./read").default);
 router.use("/create", require("./create").default);
 router.use("/delete", require("./delete").default);
+router.use("/edit", require("./edit").default);
 
 export default router;
