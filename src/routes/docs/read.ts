142f27dc5b55943ca6574ce594629ab4a832f094
PERCENTZERO <percentzero@outlook.com>
2023-04-28 19:42:15 +0900

    checkAuth -> getAuth

--- src/routes/docs/read.ts
+++ src/routes/docs/read.ts
@@ -2,16 +2,16 @@ import { Request, Response, Router } from "express";
 import { db } from "../../global";
 const router = Router();
 import { Document } from "../../types/document";
-import { checkAuth } from "../../middlewares/checkAuth";
 import { checkPermission } from "../../utils/checkPermission";
+import { getAuth } from "../../middlewares/getAuth";
 
-router.get("/:name", checkAuth, async (req: Request, res: Response) => {
+router.get("/:name", getAuth, async (req: Request, res: Response) => {
   const doc: Document = await db("documents")
     .select()
     .where("name", req.params.name)
     .first();
   if (doc) {
-    if (await checkPermission(req.user.id, doc.read)) {
+    if (await checkPermission(req.user.id || 3, doc.read)) {
       res.status(200).json({ ok: true, doc });
     } else {
       res.status(403).json({ ok: false, status: "Docs:2" });
