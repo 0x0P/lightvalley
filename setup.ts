61d01ec5a8a9f75919ac2f85a093fcd64bc770cf
PERCENTZERO <percentzero@outlook.com>
2023-04-28 19:42:05 +0900

    셋업 메뉴 추가

--- setup.ts
+++ setup.ts
@@ -1,55 +1,104 @@
 import fs from "fs";
-import * as readline from "node:readline";
+import * as readline from "readline";
 import bcrypt from "bcrypt";
 import { User, permission } from "./src/types/user";
 import { db } from "./src/global";
 import { makeKey } from "./src/utils/makeKey";
-
-console.log(`✨Lightvalley SETUP✨`);
+import { Document, documentTypes } from "./src/types/document";
+console.log("✨ Lightvalley SETUP ✨");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const q = [
-  "소유자 계정 이름(lightvalley) : ",
-  "소유자 계정 태그 : ",
-  "소유자 계정 비밀번호 : ",
-];
-
 const ask = async (questions: Array<string>): Promise<Array<string>> => {
   const answers: Array<string> = [];
   for (const question of questions) {
-    const answer = await new Promise<string>((r) => {
-      rl.question(question, r);
+    const answer = await new Promise<string>((resolve) => {
+      rl.question(question, resolve);
     });
     answers.push(answer);
   }
-  rl.close();
   return answers;
 };
 
-ask(q)
-  .then(async (answers) => {
-    const [name, tag, pw] = answers;
-    if (!pw) return console.log("비밀번호가 없어요.");
+const keySetup = async () => {
+  const accessToken = makeKey(1000);
+  const refreshToken = makeKey(1000);
+  const envContent = `ACCESS=${accessToken}\nREFRESH=${refreshToken}`;
+  fs.writeFileSync(".env", envContent);
+  console.log("✨ 키 생성 완료 ✨");
+  await main();
+};
+
+const createRootUser = async () => {
+  const questions = [
+    "소유자 계정 이름(lightvalley): ",
+    "소유자 계정 태그: ",
+    "소유자 계정 비밀번호: ",
+  ];
+  try {
+    const [name = "lightvalley", tag = "0000", pw = ""] = await ask(questions);
+    if (!pw) throw new Error("비밀번호가 없어요.");
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(pw, salt);
     const user: User = {
-      name: name || "lightvalley",
-      tag: tag || "0000",
-      identifier: (name || "lightvalley") + "#" + (tag || "0000"),
+      name,
+      tag,
+      identifier: `${name}#${tag}`,
       password: hash,
       time: new Date(),
       permission: permission.admin,
     };
     await db("users").insert(user);
-    console.log("✨소유자 계정 생성 완료✨");
-    process.exit();
-  })
-  .catch((error) => {
+    console.log("✨ 소유자 계정 생성 완료 ✨");
+  } catch (error) {
     console.error(error);
-  });
+  } finally {
+    rl.close();
+    process.exit();
+  }
+};
 
-fs.writeFileSync(".env", `ACCESS=${makeKey(1000)}\nREFRESH=${makeKey(1000)}`);
+const createIndexPage = async () => {
+  const document: Document = {
+    version: 1,
+    type: documentTypes.DOCUMENT,
+    author: "lightvalley",
+    name: "index",
+    identifier: `DOCUMENT:index:1`,
+    displayname: "INDEX",
+    content: "Hello, World!",
+    time: new Date(),
+    read: 3,
+    edit: 0,
+  };
+  await db("documents").insert(document);
+  await main();
+};
+
+const main = async () => {
+  rl.question(
+    "1. JWT 키 생성\n2. 소유자 계정 생성\n3. Index 문서 생성\n",
+    async (answer: string) => {
+      switch (answer) {
+        case "1":
+          await keySetup();
+          break;
+        case "2":
+          await createRootUser();
+          break;
+        case "3":
+          await createIndexPage();
+          break;
+        default:
+          console.log("잘못된 입력입니다.");
+          rl.close();
+          break;
+      }
+    }
+  );
+};
+
+main();
